da1e1ce39f60b31c150bd1e903d5947ad1c69327
Math-O5 <mathfernandes@usp.br>
2019-06-20 08:17:28 -0500

    url changes

--- acolhe/urls.py
+++ acolhe/urls.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from acolhe.views import geral, anfitriao, acolhido
 
 urlpatterns = [
    path('', geral.home_view, name='home'),
-   path('acolhido/', acolhido.home_acolhido, name = 'home_acolhido'),
-   path('acolhido/cadastrar', acolhido.cadastrar_view, name='cadastrar_acolhido'),
+   path('acolhido/', include(([
+      path('', acolhido.home_acolhido, name = 'home_acolhido'),
+      path('cadastrar', acolhido.cadastrar_view, name='cadastrar_acolhido'),
+   ], 'acolhe'), namespace='acolhido')
+   ),
 
-   path('anfitriao/', anfitriao.home_anfitriao, name = 'home_anfitriao'),
-   path('anfitriao/cadastrar', anfitriao.cadastrar_view, name='cadastrar_anfitriao'),
-   path('anfitriao/local', anfitriao.cadastrar_local_view, name='cadastrar_local'),
+
+   path('anfitriao/', include(([
+      path('', anfitriao.home_anfitriao, name = 'home_anfitriao'),
+      path('cadastrar', anfitriao.cadastrar_view, name='cadastrar_anfitriao'),
+      path('local', anfitriao.cadastrar_local_view, name='cadastrar_local'),
+   ], 'acolhe'),namespace='anfitriao')
+   ),
    
    path('login/', geral.login_view, name='login'),
    path('logout/', geral.logout_view, name='logout'),
